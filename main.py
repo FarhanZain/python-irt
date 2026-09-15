0d69e2dd213ac9d77d255ce349cd30d08bdcc34f
Farhan Zain <farhanabdurrahmanzain@gmail.com>
2026-09-16 02:02:55 +0700

    ganti nama db

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_db_connection():
         host="localhost",
         user="root",
         password="",
-        database="irtpl2"
+        database="irtpl"
     )
 
 # --- FUNGSI HELPER IRT ---
